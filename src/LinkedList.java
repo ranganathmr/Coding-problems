73f0bca7556b813c2690e59c336c49f0083027cb
Ranganath Mysore Radhakrishna <rmysoreradhakrishna@expedia.com>
2016-02-18 22:46:23 -0800

    Moving files into one directory structure

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -76,4 +76,28 @@ public class LinkedList<T> {
 			print(node.getNext());
 		}
 	}
+	
+	public static class Node<T> {
+		
+		private T data;
+		
+		private Node<T> next;
+
+		public T getData() {
+			return data;
+		}
+
+		public void setData(T data) {
+			this.data = data;
+		}
+
+		public Node<T> getNext() {
+			return next;
+		}
+
+		public void setNext(Node<T> next) {
+			this.next = next;
+		}
+	}
+
 }
