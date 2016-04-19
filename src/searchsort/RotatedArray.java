f8144c05c976b40c5d49fadebe8420bf1ada9f9b
Ranganath Mysore Radhakrishna <rmysoreradhakrishna@expedia.com>
2016-04-19 10:04:01 -0700

    Change method name

--- src/searchsort/RotatedArray.java
+++ src/searchsort/RotatedArray.java
@@ -3,19 +3,20 @@ package searchsort;
 public class RotatedArray {
 
 	public static void main(String[] args) {
-		int[] a = {30, 35, 40, 50, 400, 20000, -5, -4, -3, -2, -1, 0, 1, 3, 4, 5, 6, 7};
-		System.out.println(search(a, a.length - 1, 0));
+		int[] a = {30, 35, 40, 50, 400, 20000, 30000, -5, -4, -3, -2, -1, 0, 1, 3, 4, 5, 6, 7};
+		System.out.println(findPivotNumber(a, a.length - 1, 0));
 	}
 	
-	private static int search(int[] a, int h, int l) {
+	private static int findPivotNumber(int[] a, int h, int l) {
 		if(h - l > 1) {
 			int m = l + (h-l)/2;
 			if(a[l] > a[m]) {
-				return search(a, m, l);
+				return findPivotNumber(a, m, l);
 			} else {
-				return search(a, h, m);
+				return findPivotNumber(a, h, m);
 			}
 		}
 		return a[l] > a[h] ? a[l] : a[h];
 	}
+	
 }
